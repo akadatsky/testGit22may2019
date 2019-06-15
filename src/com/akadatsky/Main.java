5d669c9e6d9c438ff9a260f2b8243a0c38fe8c57
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2019-06-15 14:46:41 +0300

    asdf

--- src/com/akadatsky/Main.java
+++ src/com/akadatsky/Main.java
@@ -10,4 +10,9 @@ public class Main {
     private static void test() {
         System.out.println("Hello git");
     }
+
+    private static void test2() {
+        System.out.println("Hello git");
+    }
+
 }
